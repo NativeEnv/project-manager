74c7f19d4bdf542315c2fb7dc11414787d518320
Native Env <native.env@outlook.com>
2020-05-16 23:00:00 +0800

    Added tests

---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectTest extends TestCase
+{
+    /**
+     * Create project
+     * Endpoint http://site.local/api/v1/projects
+     * @method POST
+     */
+    public function testCreate()
+    {
+        
+    }
+}
