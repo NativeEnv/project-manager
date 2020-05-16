219df0414c47b0176d7159f4727cdc3bc261c3ff
Native Env <native.env@outlook.com>
2020-05-16 21:56:53 +0800

    Added AuthController

---
+++ app/Http/Controllers/API/v1/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+
+class AuthController extends Controller
+{
+    /**
+     * http://site.local/api/v1/auth/login
+     * @method POST
+     *
+     * @param UserLoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(UserLoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if(!$token = auth()->attempt($credentials))
+        {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * http://site.local/api/v1/auth/refresh
+     * @method POST
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh(Request $request)
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * http://site.local/api/v1/auth/logout
+     * @method POST
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+}
