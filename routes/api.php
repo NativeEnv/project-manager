bbbb814beada37f8c17787f63b369739e6c1e0eb
Native Env <native.env@outlook.com>
2020-05-16 21:58:41 +0800

    Added routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * v1 rest version
+ */
+Route::group(['prefix' => 'v1'], function() {
+    /**
+     * Auth
+     */
+    Route::group(['prefix' => 'auth'], function() {
+        Route::post('login', 'API\v1\AuthController@login');
+        Route::post('refresh', 'API\v1\AuthController@refresh')->middleware('auth:api');
+        Route::post('logout', 'API\v1\AuthController@logout')->middleware('auth:api');
+    });
+
+    /**
+     * Users
+     */
+    Route::group(['prefix' => 'users'], function() {
+        Route::post('', 'API\v1\UserController@create');
+        Route::get('{id}', 'API\v1\UserController@show')->where('id', '\d+')->middleware('auth:api');
+    });
+});
