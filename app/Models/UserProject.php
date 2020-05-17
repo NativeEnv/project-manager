0426c328b736ca10dbac800d2e747885a4c58a86
Native Env <native.env@outlook.com>
2020-05-17 18:46:25 +0800

    Added method: findUserProject

--- app/Models/UserProject.php
+++ app/Models/UserProject.php
@@ -6,10 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 use App\User;
 use App\Models\Project;
 
+/**
+ * Class UserProject
+ *
+ * @property integer $status
+ *
+ * @package App\Models
+ */
 class UserProject extends Model
 {
+    const CREATOR_STATUS = 2;
+    const MODERATOR_STATUS = 1;
+    const USER_STATUS = 0;
+
     protected $table = 'user_project';
 
+    protected $fillable = [
+        'id_user', 'id_project', 'status'
+    ];
+
+    public static function findUserProject(User $user, Project $project)
+    {
+        return self::where('id_user', '=', $user->id)->
+                     where('id_project', '=', $project->id)->
+                     first();
+    }
+    
     /**
      * @param User $user
      * @param \App\Models\Project $project
@@ -17,8 +39,6 @@ class UserProject extends Model
      */
     public static function userIsParticipant(User $user, Project $project) : bool
     {
-        return self::where('id_user', '=', $user->id)->
-                     where('id_project', '=', $project->id)->
-                     count() > 0;
+        return self::findUserProject($user, $project) != null;
     }
 }
