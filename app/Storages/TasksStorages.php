928c2db01f193acf0ea9e55bf28b3202253d0cde
Native Env <native.env@outlook.com>
2020-05-17 19:40:59 +0800

    Added TasksStorage

---
+++ app/Storages/TasksStorages.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Storages;
+
+use App\Storages\IStorage;
+
+use App\Models\Task;
+
+/**
+ * Class TasksStorages
+ * @package App\Storages
+ */
+class TasksStorages implements IStorage
+{
+    /**
+     * @var array[Task]
+     */
+    private $_tasks;
+
+    /**
+     * TasksStorages constructor.
+     *
+     * @param $id_project
+     */
+    public function __construct($id_project)
+    {
+        $this->_tasks = Task::where('id_project', $id_project)->get();
+    }
+
+    /**
+     * @return array
+     */
+    public function get()
+    {
+        return $this->_tasks;
+    }
+
+    /**
+     * @param $id
+     * @return object|null
+     */
+    public function find($id)
+    {
+        foreach($this->_tasks as $task)
+        {
+            if($task->id == $id) return $task;
+        }
+        return null;
+    }
+}
