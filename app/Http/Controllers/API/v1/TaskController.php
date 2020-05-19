ec6b5ae06e7ed8a87ab224d7ab596c332fe7b1e9
Native Env <native.env@outlook.com>
2020-05-20 02:07:05 +0800

    Added action: index

--- app/Http/Controllers/API/v1/TaskController.php
+++ app/Http/Controllers/API/v1/TaskController.php
@@ -10,6 +10,15 @@ use App\Http\Requests\TaskCreateRequest;
 
 class TaskController extends Controller
 {
+    public function index($id_project)
+    {
+        $project = Project::findOrFail($id_project);
+
+        return $this->jsonResponse([
+            'tasks' => $project->getTasks()->all()
+        ]);
+    }
+
     /**
      * http://site.local/api/v1/projects/{id_project}/tasks
      * @method POST
@@ -31,37 +40,26 @@ class TaskController extends Controller
     }
 
     /**
-     * Show all tasks:
-     *  http://site.local/api/v1/projects/{id_project}/tasks
-     *
-     * Show the task:
-     *  http://site.local/api/v1/projects/{id_project}/tasks/{id_task}
+     * http://site.local/api/v1/projects/{id_project}/tasks/{$id_task}
      *
      * @param $id_project
      * @param null $id_task
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
      */
-    public function show($id_project, $id_task = null)
+    public function show($id_project, $id_task)
     {
         $project = Project::findOrFail($id_project);
 
         if(!$project->userHasAccess(auth()->user())) $this->responsePermissionDenied();
 
-        if($id_task != null)
+        if(!$task = $project->getTasks()->find($id_task))
         {
-            if(!$task = $project->getTasks()->find($id_task))
-            {
-                return $this->responseNotFound();
-            }
-
-            return $this->jsonResponse([
-                'task' => $task
-            ]);
+            return $this->responseNotFound();
         }
 
         return $this->jsonResponse([
-            'tasks' => $project->getTasks()->all()
+            'task' => $task
         ]);
     }
 }
